5e65d6ca49949a74d255cbb310c5ed4a562f9e41
Kevin <kevinsadao@hotmail.com>
2023-11-12 17:42:53 -0300

    Adicionado exclusão, alteração e finalização de tarefa

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Button } from 'react-bootstrap';
-import api from '../../services/api';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import { Table, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import api from "../../services/api";
+import moment from "moment";
+import "./index.css";
+
 interface ITask {
   id: number;
   title: string;
@@ -10,24 +13,58 @@ interface ITask {
   created_at: Date;
   updated_at: Date;
 }
-const Tasks: React.FC = () => {
-  const [tasks, setTasks] = useState<ITask[]>([])
-  useEffect(() => {
-    loadTasks()
-  }, [])
-  async function loadTasks() {
-    const response = await api.get('/tasks')
-    console.log(response);
-    setTasks(response.data)
-  }
-  function formatDate(date: Date) {
-    return moment(date).format('DD/MM/YYYY')
-  }
-  return (
 
+const Tasks: React.FC = () => {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  async function loadTasks() {
+    const response = await api.get("/tasks");
+
+    console.log(response);
+
+    setTasks(response.data);
+  }
+
+  function formatDate(date: Date) {
+    return moment(date).format("DD/MM/YYYY");
+  }
+
+  function newTask() {
+    navigate("/tarefas/cadastro");
+  }
+
+  async function finishedTask(id: number) {
+    await api.patch(`/tasks/${id}`);
+    loadTasks();
+  }
+
+  async function deleteTask(id: number) {
+    await api.delete(`/tasks/${id}`);
+    loadTasks();
+  }
+
+  function editTask(id: number) {
+    navigate(`/tarefas/cadastro/${id}`);
+  }
+
+  function viewTask(id: number) {
+    navigate(`/tarefas/${id}`);
+  }
+
+  return (
     <div className="container">
       <br />
-      <h1>Página de Tarefas</h1>
+      <div className="task-header">
+        <h1>Tarefas</h1>
+        <Button variant="dark" size="sm" onClick={newTask}>
+          Nova Tarefa
+        </Button>
+      </div>
       <br />
       <Table striped bordered hover className="text-center">
         <thead>
@@ -39,26 +76,52 @@ const Tasks: React.FC = () => {
             <th>Ações</th>
           </tr>
         </thead>
+
         <tbody>
-          {
-            tasks.map(task => (
-              <tr key={task.id}>
-                <td>{task.id}</td>
-                <td>{task.title}</td>
-                <td>{formatDate(task.updated_at)}</td>
-                <td>{task.finished ? "Finalizado" : "Pendente"}</td>
-                <td>
-                  <Button size="sm" variant="primary">Editar</Button>{' '}
-                  <Button size="sm" variant="success">Finalizar</Button>{' '}
-                  <Button size="sm" variant="warning">Visualizar</Button>{' '}
-                  <Button size="sm" variant="danger">Remover</Button>{' '}
-                </td>
-              </tr>
-            ))
-          }
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.id}</td>
+              <td>{task.title}</td>
+              <td>{formatDate(task.updated_at)}</td>
+              <td>{task.finished ? "Finalizado" : "Pendente"}</td>
+              <td>
+                <Button
+                  size="sm"
+                  variant="primary"
+                  disabled={task.finished}
+                  onClick={() => editTask(task.id)}
+                >
+                  Editar
+                </Button>{" "}
+                <Button
+                  size="sm"
+                  variant="success"
+                  disabled={task.finished}
+                  onClick={() => finishedTask(task.id)}
+                >
+                  Finalizar
+                </Button>
+                <Button
+                  size="sm"
+                  variant="warning"
+                  onClick={() => viewTask(task.id)}
+                >
+                  Visualizar
+                </Button>{" "}
+                <Button
+                  size="sm"
+                  variant="danger"
+                  onClick={() => deleteTask(task.id)}
+                >
+                  Remover
+                </Button>{" "}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
   );
-}
+};
+
 export default Tasks;
