4e1fc75e62ba4a2255570b1e8050ec0f7185ec0d
Kevin <kevinsadao@hotmail.com>
2023-11-12 16:17:45 -0300

    Adicionado backend

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+import Tasks from "./pages/Tasks";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tarefas" element={<Tasks />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
